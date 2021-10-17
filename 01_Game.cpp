2c793b708d6895b902e2408fd87a16ff94e3ecb1
Sayan kar <sayankar1308@gmail.com>
2021-10-17 23:36:32 +0530

    Update 01_Game.cpp

--- 01_Game.cpp
+++ 01_Game.cpp
@@ -3,21 +3,17 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
-    {
+    cin >> t;
+    while(t--) {
         string s;
-        cin>>s;
-        int ones=0,zeros=0;
-        int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='1')
-            {
+        cin >> s;
+        int ones = 0, zeros = 0;
+        int n = s.size();
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '1') {
                 ones++;
             }
-            if(s[i]=='0')
-            {
+            if(s[i]=='0') {
                 zeros++;
             }
         }
